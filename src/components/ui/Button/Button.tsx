b9f0d3c673909198f8e35f5c5883fcf9a0fb27da
PoweredDeveloper <orokana.otaku.dev@gmail.com>
2025-11-28 12:47:04 +0300

    feat: update CVA; add dynamic compound classes support; add tanstack table dependency

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -2,40 +2,54 @@ import { forwardRef, type ButtonHTMLAttributes } from 'react'
 import cva, { type VariantProps } from '@/lib/cva'
 import { cn } from '@/lib/cn'
 
-export const buttonVariants = cva(
-  'font-semibold text-center rounded-lg cursor-pointer transition-colors duration-100 h-min w-min text-nowrap border select-none',
-  {
-    variants: {
-      size: {
-        sm: 'text-sm px-3 py-1.5',
-        md: 'text-sm px-4 py-2',
-        lg: 'text-base px-6 py-3',
-        xl: 'text-lg px-8 py-4',
-      },
-      variant: {
-        primary: 'bg-__COLOR__ text-__COLOR__-foreground border-__COLOR__ hover:bg-__COLOR__/85 hover:border-__COLOR__/85 active:bg-__COLOR__ active:border-__COLOR__',
-        outline: 'text-__COLOR__ border-__COLOR__ bg-transparent hover:bg-__COLOR__/10 active:bg-__COLOR__/20',
-        ghost: 'text-__COLOR__ bg-transparent border-transparent hover:bg-__COLOR__/10 active:bg-__COLOR__/20',
-        disabled: 'bg-secondary text-secondary-foreground border-secondary-foreground cursor-not-allowed',
-      },
-      color: {
-        primary: 'primary',
-        success: 'success',
-        warning: 'warning',
-        error: 'error',
-      },
+export const buttonVariants = cva('font-semibold text-center rounded-lg cursor-pointer transition-colors duration-100 h-min w-min text-nowrap border select-none', {
+  variants: {
+    size: {
+      sm: 'text-sm px-3 py-1.5',
+      md: 'text-sm px-4 py-2',
+      lg: 'text-base px-6 py-3',
+      xl: 'text-lg px-8 py-4',
     },
-    default: {
-      size: 'md',
-      variant: 'primary',
-      color: 'primary',
+    variant: {
+      primary: '',
+      outline: 'bg-transparent',
+      ghost: 'bg-transparent border-transparent',
+      disabled: 'bg-secondary text-secondary-foreground border-secondary-foreground cursor-not-allowed',
+    },
+    color: {
+      primary: 'primary',
+      success: 'success',
+      warning: 'warning',
+      error: 'error',
     },
   },
-  '__COLOR__',
-)
+  compoundVariants: [
+    {
+      variant: 'primary',
+      class: ({ color }) =>
+        `bg-${color} text-${color}-foreground border-${color}
+           hover:bg-${color}/85 hover:border-${color}/85
+           active:bg-${color} active:border-${color}`,
+    },
+    {
+      variant: 'outline',
+      class: ({ color }) =>
+        `text-${color} border-${color}
+           hover:bg-${color}/10 active:bg-${color}/20`,
+    },
+    {
+      variant: 'ghost',
+      class: ({ color }) => `text-${color} hover:bg-${color}/10 active:bg-${color}/20`,
+    },
+  ],
+  default: {
+    size: 'md',
+    variant: 'primary',
+    color: 'primary',
+  },
+})
 
 export type ButtonVariantProps = VariantProps<typeof buttonVariants>
-
 export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & ButtonVariantProps
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ children, variant, size, color, className, disabled, ...props }, ref) => {
--- src/lib/cva.ts
+++ src/lib/cva.ts
@@ -1,8 +1,8 @@
 type CompoundVariant<V extends Record<string, Record<string, string>>> = Partial<{ [K in keyof V]: keyof V[K] }> & {
-  class: string
+  class: string | ((props: { [K in keyof V]: keyof V[K] }) => string)
 }
 
-type CvaOptions<V extends Record<string, Record<string, string>> = {}, D extends Partial<{ [K in keyof V]: keyof V[K] }> = {}> = {
+export type CvaOptions<V extends Record<string, Record<string, string>>, D extends Partial<{ [K in keyof V]: keyof V[K] }> = {}> = {
   variants?: V
   default?: D
   compoundVariants?: CompoundVariant<V>[]
@@ -10,17 +10,22 @@ type CvaOptions<V extends Record<string, Record<string, string>> = {}, D extends
 
 /**
  * Creates a class-variant function that maps variant props to CSS classes.
- * Using color variant replaces color in entire class i.e. for given `bg-__COLOR__/20` and `color: "success"`
- * It returns `bg-success/20`
  *
  * @template V A record describing all variant groups and their possible values
  * @template D A record describing default variant values for the variant groups in V
  *
  * @param {string} base Base CSS class applied to every output
  * @param {{ variants?: V; default?: D }} options options with variants and default values
- * - `variants`: A mapping of variant names to allowed variant values and CSS classes. Adding color rewrites `primary` with given color
- * - `default`: The default variant values applied if the user does not specify them.
+ * - `variants`: A mapping of variant names to allowed variant values and CSS classes.
+ * - `default`: The default variant values applied if the user does not specify them. Class can use functions with variants to produce dyncamic classes
  * - `compoundVariants`: Specific class on some variants.
+ * @example
+ *  compoundVariants: [
+ *    {
+ *      variant: 'ghost',
+ *      class: ({ color }) => `text-${color} hover:bg-${color}/10 active:bg-${color}/20`,
+ *    },
+ *  ],
  *
  * @returns {(props?: { [K in keyof V]?: keyof V[K] } & { className?: string }) => string} Returns a function that accepts variant props and returns a final class string.
  *
@@ -41,7 +46,6 @@ type CvaOptions<V extends Record<string, Record<string, string>> = {}, D extends
 export default function cva<V extends Record<string, Record<string, string>>, D extends Partial<{ [K in keyof V]: keyof V[K] }>>(
   base: string,
   options: CvaOptions<V, D>,
-  replaceColor: string = 'primary',
 ): (props?: { [K in keyof V]?: keyof V[K] } & { className?: string }) => string {
   return function func(
     props: {
@@ -49,22 +53,15 @@ export default function cva<V extends Record<string, Record<string, string>>, D 
     } & { className?: string } = {},
   ) {
     let classes = [base]
-    const finalProps = { ...options.default, ...props }
+    const finalProps = { ...options.default, ...Object.fromEntries(Object.entries(props).filter(([, v]) => v !== undefined)) }
 
     for (const variantName in options.variants) {
-      if (variantName === 'color') continue
       const variantValue = finalProps[variantName]
       const variantMap = options.variants[variantName]
 
       if (variantValue && variantMap[variantValue]) {
         classes.push(variantMap[variantValue])
       }
-    }
-
-    // Replace colors
-    const selectedColor = (finalProps.color ? finalProps.color : options.default?.color) as string
-    if (selectedColor) {
-      classes = classes.map((c) => c.replace(new RegExp(replaceColor, 'g'), selectedColor))
     }
 
     if (options.compoundVariants) {
@@ -80,7 +77,8 @@ export default function cva<V extends Record<string, Record<string, string>>, D 
         }
 
         if (matches) {
-          classes.push(compound.class)
+          const c = compound.class
+          classes.push(typeof c === 'function' ? c(finalProps as any) : c)
         }
       }
     }
