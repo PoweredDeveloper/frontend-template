bfc6369535156b641ed648789a94bdb0e036a63a
PoweredDeveloper <orokana.otaku.dev@gmail.com>
2025-11-28 20:56:37 +0300

    feat: add query client

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -1,4 +1,4 @@
-import ThemeToggle from '@/components/ui/ThemeToggle'
+import ThemeToggle from '@/components/ui/themeToggle'
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 
 const RootLayout = () => (
